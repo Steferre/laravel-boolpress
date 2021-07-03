ca76cdde309efc1faae6ef81446ccca5da697e13
Steferre <ferrera.ste@gmail.com>
2021-07-03 17:55:16 +0200

    aggiunta tabella categorie e compilata con seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['sport', 'food', 'news', 'lifestyle', 'gossip'];
+
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->slug = Str::slug($category);
+            $newCategory->save();
+        }
+    }
+}
